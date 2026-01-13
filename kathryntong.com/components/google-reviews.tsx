b67770957c6ee698c93a1b0e43260f5dfad429a4
Kathryn Tong <kathryn@camobileapostille.com>
2026-01-13 13:07:59 -0800

    Adjust Google Reviews widget spacing to ensure it's at bottom before red CTA boxes

--- kathryntong.com/components/google-reviews.tsx
+++ kathryntong.com/components/google-reviews.tsx
@@ -4,7 +4,7 @@ import Script from "next/script"
 
 export default function GoogleReviews() {
   return (
-    <div className="py-8">
+    <div className="mb-8">
       <div id="ti-widget-container-042341d620001924e5261a9e078"></div>
       <Script
         src="https://cdn.trustindex.io/loader.js?042341d620001924e5261a9e078"
